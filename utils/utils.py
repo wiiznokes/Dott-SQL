043378136a9c0f173bcc7681795f29ee503732b9
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-04-25 19:00:16 +0200

    support horaire

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 
 import os
 
+import datetime
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -14,3 +15,7 @@ def remove_file(file):
 
 def to_sql_string(chaine: str):
     return f"'{chaine}'"
+
+
+def chaine_is_valid(chaine: str) -> bool:
+    return  chaine and not chaine.isspace()
