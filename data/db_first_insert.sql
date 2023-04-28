a3cf3a5e3da5118dcfb4912e8a2eb41a0b5aca71
mislaidis <mislaidis@gmail.com>
2023-04-28 14:36:18 +0200

    hhjghgffh

--- data/db_first_insert.sql
+++ data/db_first_insert.sql
@@ -7,19 +7,15 @@ VALUES (1, "Montaigné", "Gilbert", '1990-03-04', "38 allée d'Ahm");
 INSERT INTO Clients 
 VALUES (2, "Rousseau", "Lénaic", '2002-11-04', "38 allée d'Ahm");
 
-
 INSERT INTO Clients 
 VALUES (3, "Garcia", "Giulio", '2000-08-06', "15 Gabriel Peri");
 
 INSERT INTO Clients 
 VALUES (4, "Salina", "Martha", '2003-07-02', "13 Rue de la Republique");
 
-
-
 INSERT INTO Employes 
 VALUES (1, "Montaigné", "Albert", '1991-03-04', "37 allée d'Ahm", 
 "10h30,12h00 | 13h30,17h30 : 13h30,17h30 | : | : | 13h30,17h30 : |13h30,17h30 : |: |");
-
 
 INSERT INTO Employes 
 VALUES (2, "Perez", "Rafaela", '2000-03-05', "14 Rue Colonel", 
@@ -45,7 +41,6 @@ VALUES ("xiaomi", "rouge", 25, 10, 6);
 INSERT INTO Modeles 
 VALUES ("MOBYGUM", "bleu", 65, 12, 9);
 
-
 INSERT INTO Trottinettes 
 VALUES (1, '2019-03-04', "bon", "fast", 1);
 
@@ -57,20 +52,25 @@ VALUES (3, '2012-01-01', "moyen", "MOBYGUM", 3);
 
 
 INSERT INTO Locations 
-VALUES (1, '2022-03-04 17:50', "77 rue allié", NULL, NULL, NULL, 1, 1);
+VALUES (1, '2022-03-04 17:50', "77 rue allié", NULL, NULL, 1, 1);
 
 
 INSERT INTO Locations 
-VALUES (2, '2022-03-04 18:50', "77 rue allié", "88 place du peuple", "2022-03-04 20:20", 19.2, 1, 1);
-
+VALUES (2, '2022-03-04 18:50', "77 rue allié", "88 place du peuple", "2022-03-04 20:20", 1, 1);
 
 INSERT INTO Locations 
 VALUES (3, '2022-03-04 13:00', "77 rue allié", "88 place du peuple", "2022-03-04 15:00", 4, 2);
-
 
 INSERT INTO LocationsTrottinettes 
 VALUES (1, 1);
 
 INSERT INTO LocationsTrottinettes 
-VALUES (2, 2);
+VALUES (2, 1);
 
+INSERT INTO LocationsTrottinettes 
+VALUES (3, 2);
+
+INSERT INTO LocationsTrottinettes 
+VALUES (3, 1);
+
+---DELETE FROM LocationsTrottinettes WHERE numero_location=3 AND numero_trottinette=1;
